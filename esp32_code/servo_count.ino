3495819667071039fe90675ecab0dac0e993564e
vishy-ui <patelvishwas175@gmail.com>
2026-04-12 14:22:22 +0530

    Revise servo angle mapping for better control
    Updated servo control to map 5 states to 180 degrees, with 45-degree increments.

--- esp32_code/servo_count.ino
+++ esp32_code/servo_count.ino
@@ -6,16 +6,18 @@ int servoPin = 18;
 void setup() {
   Serial.begin(9600);
   myServo.attach(servoPin);
-  myServo.write(0);   // default
+  myServo.write(0);   // Default to 0 degrees (Closed Fist)
 }
 
 void loop() {
   if (Serial.available()) {
     char c = Serial.read();
 
-    if (c == '1') myServo.write(0);
-    if (c == '2') myServo.write(60);
-    if (c == '3') myServo.write(120);
+    // Mapping 5 states to 180 degrees (45 degrees per finger)
+    if (c == '0') myServo.write(0);
+    if (c == '1') myServo.write(45);
+    if (c == '2') myServo.write(90);
+    if (c == '3') myServo.write(135);
     if (c == '4') myServo.write(180);
   }
 }
